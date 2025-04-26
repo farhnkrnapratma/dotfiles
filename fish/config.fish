80be3489311b2cb99de7dbfe8ee8f7a033e15d3e
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-04-27 06:47:07 +0700

    Patch: Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,75 @@
+function fish_greeting
+    fastfetch
+    echo "Hello, $(whoami)!"
+end
+
+export "MICRO_TRUECOLOR=1"
+
+if type nvim >/dev/null
+    set -Ux EDITOR nvim
+    set -Ux VISUAL nvim
+else if type helix >/dev/null
+    set -Ux EDITOR helix
+    set -Ux VISUAL helix
+else if type hx >/dev/null
+    set -Ux EDITOR hx
+    set -Ux VISUAL hx
+else
+    set -Ux EDITOR nano
+    set -Ux VISUAL nano
+end
+
+set os_name (awk -F= '$1=="NAME" {gsub(/"/, "", $2); print $2}' /etc/os-release)
+
+if test "$os_name" = "Arch Linux"
+    alias pacup="sudo pacman -Syu"
+    alias pacin="sudo pacman -S"
+    alias pacrm="sudo pacman -Rns"
+    alias pacrd="sudo pacman -Rdd"
+    alias paccl="sudo pacman -Rs $(pacman -Qdtq)"
+    alias paruup="paru -Syu"
+    alias paruin="paru -S"
+    alias parurm="paru -Rns"
+    alias parurd="paru -Rdd"
+    alias parucl="paru -Rs $(paru -Qdtq)"
+    alias u="paru -Syuu"
+end
+
+alias d="sudo rm -rf"
+alias b="z .."
+alias x="exit"
+alias c="clear"
+alias l="eza -l"
+alias la="eza -la"
+alias e="$EDITOR"
+alias fishc="e ~/.config/fish/config.fish && source ~/.config/fish/config.fish"
+
+function i
+    sudo pacman -S $argv 2>/dev/null
+    if test $status -ne 0
+        paru -S $argv
+    end
+end
+
+function r
+    sudo pacman -Rns $argv 2>/dev/null
+    if test $status -ne 0
+        paru -Rns $argv
+    end
+end
+
+function del-locate
+    locate $argv[1] | sudo xargs -d '\n' rm -rf
+    sudo updatedb
+end
+
+if type cargo >/dev/null
+    alias crun="cargo run"
+    alias cbld="cargo build"
+    alias ctst="cargo test"
+    alias chck="cargo check"
+end
+
+set PATH $PATH /home/farhnkrnapratma/.local/bin
+
+zoxide init fish | source
